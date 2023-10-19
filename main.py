e39a78937bbfcc6a308f87883742d91f0f668f4d
QWhitewalker <gion.alfanz@hotmail.com>
2023-10-19 13:11:17 +0000

    added replacement of unreadable characters

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def regex_validator():
     else:
         print("nomatch")
 
-with open(config["csv-file"]) as csv_file:
+with open(config["csv-file"],encoding=config["encoding"] ,errors="replace") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=config["delimiter"])
     line_count = 0
     for row in csv_reader:
