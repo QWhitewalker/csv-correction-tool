d5793d24bb5778af25d496e6f355c025f5c38452
QWhitewalker <gion.alfanz@hotmail.com>
2023-10-19 13:15:08 +0000

    moved validator function into external module

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import csv
 import re
 import json
+import validator
 
 # load config.json into python dictionary "config"
 configFile = open('config.json')
 config = json.load(configFile)
 
 pattern = re.compile(config["columns"]["1"])
-
-def regex_validator():
-    if pattern.match(row[1]):
-        print("match")
-    else:
-        print("nomatch")
 
 with open(config["csv-file"],encoding=config["encoding"] ,errors="replace") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=config["delimiter"])
@@ -23,7 +18,7 @@ with open(config["csv-file"],encoding=config["encoding"] ,errors="replace") as c
             line_count += 1
         else:
             print(row[1])
-            regex_validator()
+            validator.regex_validator(row, pattern)
             line_count += 1
             #if line_count == 5:
                 #break
--- validator.py
+++ validator.py
@@ -1,2 +1,8 @@
 def validate_charset():
     pass
+
+def regex_validator(row, pattern):
+    if pattern.match(row[1]):
+        print("match")
+    else:
+        print("nomatch")
