b9af6bcb26147153b7b5c106efa36c6e62127282
QWhitewalker <gion.alfanz@hotmail.com>
2023-10-23 20:01:01 +0000

    added output-delimiter to config.json

--- main.py
+++ main.py
@@ -17,20 +17,20 @@ if os.path.isfile(config['output-file']):
 pattern = re.compile(config['columns']['1'])
 
 # open input file and call all other needed functions to process the file
-with open(config['input-file'], encoding=config['encoding'], errors='replace') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=config['delimiter'])
+with open(config['input-file'],encoding=config['encoding'],errors='replace') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=config['input-delimiter'])
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            writer.write_row(row,config['output-file'],config['encoding'])
+            writer.write_row(row,config['output-file'],config['encoding'],config['output-delimiter'])
             line_count += 1
         else:
-            writer.write_row(row,config['output-file'],config['encoding'])
+            writer.write_row(row,config['output-file'],config['encoding'],config['output-delimiter'])
             #validator.regex_validator(row, pattern)
             line_count += 1
             if line_count == 5:
                 break
-    print(f'Processed {line_count} lines.')
+print(f'Processed {line_count} lines.')
 #print(config['columns'].keys())
 
 def regex_corrector():
--- writer.py
+++ writer.py
@@ -1,7 +1,7 @@
 import csv
 
 # writes array into row of file (appends)
-def write_row(p_output,p_file,p_encoding):
+def write_row(p_output,p_file,p_encoding,p_delimiter):
     with open(p_file, mode='a', encoding=p_encoding) as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=';')
+        csv_writer = csv.writer(csv_file, delimiter=p_delimiter)
         csv_writer.writerow(p_output)
