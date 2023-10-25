5769f5ff3e40da10d45231750c1cb393891ee2e8
QWhitewalker <gion.alfanz@hotmail.com>
2023-10-25 19:41:19 +0000

    added error writer function idea

--- writer.py
+++ writer.py
@@ -5,3 +5,6 @@ def write_row(p_output,p_file,p_encoding,p_delimiter):
     with open(p_file, mode='a', encoding=p_encoding) as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=p_delimiter)
         csv_writer.writerow(p_output)
+
+def write_error_row(p_error,p_output,p_file,p_encoding,p_delimiter):
+    pass
